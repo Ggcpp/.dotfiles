90fde2b7277ccb67773a09da1d2adc72c2d0cd27
Ggcpp <gg.geinoz@outlook.com>
2022-01-25 17:36:27 +0100

    update

---
+++ i3/.config/i3/start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# ~/start.sh
+
+JSON_FILE=$(i3-msg -t get_workspaces)
+
+# Open workspace
+i3 workspace number "$1"
+
+NB_WS=$(jq '. | length' <<< $JSON_FILE)
+TARGET_WS="$1"
+
+WS_EXIST=false
+for ((i=0; i<$NB_WS; i++));
+do
+    CUR_WS=$(jq .[$i].num <<< $JSON_FILE)
+    if [[ "$CUR_WS" == "$TARGET_WS" ]]
+    then
+        WS_EXIST=true
+        break
+    fi
+done
+
+if [[ "$WS_EXIST" == false ]]
+then
+    case $TARGET_WS in
+        8)
+            alacritty;;
+        9)
+            chromium;;
+    esac
+fi
