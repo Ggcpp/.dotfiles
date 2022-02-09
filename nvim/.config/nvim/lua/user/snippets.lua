9bb92644ed0ebaf379e755cd7f3a0f7e09004771
Ggcpp <gg.geinoz@outlook.com>
2022-02-09 22:13:48 +0100

    update

--- nvim/.config/nvim/lua/user/snippets.lua
+++ nvim/.config/nvim/lua/user/snippets.lua
@@ -7,15 +7,24 @@ if not status_ok then
     return
 end
 
+local text = luasnip.text_node
+local insert = luasnip.insert_node
+
 luasnip.snippets = {
-    md = {
-        snip({
-            trig = "/img", 
+    markdown = {
+        luasnip.snippet({
+            trig = "\\img", 
             namr = "image",
             dscr = "Create an HTML image tag"
         },
         {
-            text({'<img src="nothing.png">'}), insert(0)
+            text('<img src="'),
+            insert(1, 'source'),
+            text('" style="width: '),
+            insert(2, '100%'),
+            text(';">'),
+            text({ "", "", ""}),
+            insert(0)
         })
     }
 }
