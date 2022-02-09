8ce63eab8145972e3b4798171958dd012c6b1c19
Ggcpp <gg.geinoz@outlook.com>
2022-02-09 22:29:03 +0100

    update

--- nvim/.config/nvim/lua/user/snippets.lua
+++ nvim/.config/nvim/lua/user/snippets.lua
@@ -7,15 +7,16 @@ if not status_ok then
     return
 end
 
+local snip = luasnip.snippet
 local text = luasnip.text_node
 local insert = luasnip.insert_node
 
 luasnip.snippets = {
     markdown = {
-        luasnip.snippet({
-            trig = "\\img", 
+        snip({
+            trig = "/img", 
             namr = "image",
-            dscr = "Create an HTML image tag"
+            dscr = "Create an HTML image tag."
         },
         {
             text('<img src="'),
@@ -24,6 +25,51 @@ luasnip.snippets = {
             insert(2, '100%'),
             text(';">'),
             text({ "", "", ""}),
+            insert(0)
+        }),
+
+        snip({
+            trig = "/imgleft",
+            namr = "image left",
+            dscr = "Create an HTML image tag floating left."
+        },
+        {
+            text('<img class="image-left" src="'),
+            insert(1, 'source'),
+            text('" style="width: '),
+            insert(2, '100%'),
+            text(';">'),
+            text({ "", "", ""}),
+            insert(0)
+        }),
+
+        snip({
+            trig = "/imgright",
+            namr = "image right",
+            dscr = "Create an HTML image tag floating right."
+        },
+        {
+            text('<img class="image-right" src="'),
+            insert(1, 'source'),
+            text('" style="width: '),
+            insert(2, '100%'),
+            text(';">'),
+            text({ "", "", ""}),
+            insert(0)
+        }),
+
+        snip({
+            trig = "/link",
+            namr = "link",
+            dscr = "Create a link in markdown syntax."
+        },
+        {
+            text("["),
+            insert(1, "link text"),
+            text("]("),
+            insert(2, "link"),
+            text(")"),
+            text({"", "", ""}),
             insert(0)
         })
     }
