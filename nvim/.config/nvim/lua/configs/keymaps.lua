3271a00b4cd9fb097afd076de67e4b3281b1ce18
Ggcpp <gg.geinoz@outlook.com>
2022-07-16 00:21:13 +0200

    update

--- nvim/.config/nvim/lua/configs/keymaps.lua
+++ nvim/.config/nvim/lua/configs/keymaps.lua
@@ -11,11 +11,15 @@ keymap("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>o", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown({ previewer = false, prompt_title = 'Open File' }))<cr>", opts)
 keymap("n", "<leader>F", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 
+-- movements
+keymap("n", "J", "<C-d>", opts) -- Scroll half a page down by default
+keymap("n", "K", "<C-u>", opts) -- Scroll half a page up by default
+
 -- split window
 keymap("n", "<leader>v", "<C-w>v", opts)
 keymap("n", "<leader>h", "<C-w>s", opts)
 
--- window navigation
+-- splits navigation
 keymap("n", "<C-p>", "<C-w>h", opts)
 keymap("n", "<C-y>", "<C-w>j", opts)
 keymap("n", "<C-e>", "<C-w>k", opts)
